3920396919a25850b60e5616f15190bcf03efbd4
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-07-30 23:14:05 +0200

    append_dict for tables.

--- src/touchutilcollection/tables.py
+++ src/touchutilcollection/tables.py
@@ -8,3 +8,8 @@ def table_to_dicts( table_op:tableDAT, key_source:Literal["row", "col"] = "row" 
         keys[ index ] :  cell.val for index ,cell in enumerate(collection)  
     } for collection in collection_source()[1:] ]
 
+def append_dict_to_table( table_op:tableDAT, source_dict:dict, key_source:Literal["row", "col"] = "row", default_value:str = "" ):
+    keys = tuple( cell.val for cell in getattr(table_op, key_source)(0) )
+    getattr( table_op, f"append{key_source.capitalize()}")([
+        source_dict.get( key, default_value) for key in keys
+    ])
