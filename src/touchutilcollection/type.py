0cfcf4729f7d01fb5f46f3a80e712d1691d3df77
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-07-30 15:21:40 +0200

    fixed typing, no need for pyright exception.

--- src/touchutilcollection/type.py
+++ src/touchutilcollection/type.py
@@ -5,7 +5,7 @@ T = TypeVar("T")
 def is_tuple(value) -> TypeGuard[Tuple]:
 	return isinstance( value, tuple )
 
-def op_as(pattern:Union[str, int, Tuple[Union[str, int], ...]], asType:Type[T], includeUtility = False): # pyright: ignore[reportInvalidTypeVarUse] 
+def op_as(pattern:Union[str, int, Tuple[Union[str, int], ...]], asType:Type[T], includeUtility = False):
 	# T is in use. This is only a utility function!
 	if is_tuple( pattern ): _pattern = pattern
 	else: _pattern = (pattern,)
@@ -14,8 +14,7 @@ def op_as(pattern:Union[str, int, Tuple[Union[str, int], ...]], asType:Type[T], 
 
 opAs = op_as
 
-def op_as_ex(pattern:Union[str, int, Tuple[Union[str, int], ...]], asType:Type[T], includeUtility = False): # pyright: ignore[reportInvalidTypeVarUse] 
-	# T is in use. This is only a utility function!
+def op_as_ex(pattern:Union[str, int, Tuple[Union[str, int], ...]], asType:Type[T], includeUtility = False): 
 	if is_tuple( pattern ): _pattern = pattern
 	else: _pattern = (pattern,)
 	return cast( T, opex(*_pattern, includeUtility = includeUtility)) # pyright: ignore[reportArgumentType]
